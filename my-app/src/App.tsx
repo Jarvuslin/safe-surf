15d505b5cea445570e51b396222c38132ed4faf8
Ghaith Khalil <ghaithkale@gmail.com>
2022-05-14 23:42:00 -0400

    updates

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Profanity from './Profanity';
+import AboutUs from "./AboutUs";
+import ContactUs from "./ContactUs";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/profanity" element={<Profanity/>}/>
+              <Route path="/about-us" element={<AboutUs/>}/>
+              <Route path="/contact-us" element={<ContactUs/>}/>
             </Routes>
           </div>
         </div>
--- my-app/src/Profanity.tsx
+++ my-app/src/Profanity.tsx
@@ -1,33 +1,41 @@
-import {useNavigate, useParams} from "react-router-dom";
 import {useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
-    const navigate = useNavigate();
 
-    const [data, setData] = useState(null)
-    const [isPending, setIsPending] = useState(false)
+const Create = () => {
+    const [link, setLink] = useState('');
+    const [isPending, setIsPending] = useState(false);
     const [error, setError] = useState(null)
 
-    const handleClick = () => {
-        fetch('http://localhost:5000/api/profanity/', {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const profanityInfo = {link};
+
+        setIsPending(true);
+
+        fetch('http://localhost:5000/api/profanity', {
             method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                link: 'testing'
-            })
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(profanityInfo)
         })
             .then(res => {
                 if (!res.ok) {
-                    throw Error('Failed to fetch profanity data.')
+                    throw new Error('Failed to POST!');
                 }
-                return res.json()
+                return res.json();
             })
-            .then((data) => {
-                console.log(data)
-
-                setData(data);
+            .then(response => {
                 setIsPending(false);
-                setError(null);
+
+                response.file().then((file: Blob | MediaSource) => {
+                    let url = window.URL.createObjectURL(file);
+                    let a = document.createElement('a');
+                    a.href = url;
+                    a.download = 'employees.json';
+                    a.click();
+                });
             })
             .catch(err => {
                 if (err.name === 'AbortError') {
@@ -37,24 +45,25 @@ const Home = () => {
                 setError(err.message);
                 setIsPending(false);
             });
-        return ''
     }
 
     return (
-        <div className="profanity-data">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {data && (
-                <article>
-                    <h1>Response</h1>
-                </article>
-            )}
-            <button onClick={() => {
-                handleClick()
-                setIsPending(true)
-            }}>Fetch</button>
+        <div className="profanity">
+            <h2>Profanity Filter</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Link:</label>
+                <input
+                    type="text"
+                    required
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                />
+                {!isPending && <button>Submit</button>}
+                {isPending && <button disabled>Fetching Info...</button>}
+                {error && <p>{error}</p>}
+            </form>
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Create;
