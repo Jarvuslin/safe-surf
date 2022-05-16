6b1ca3734c1fcfd97c44bd82379a883b9e5bcfa8
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-05-15 21:40:27 -0400

    file retrieval

--- my-app/src/Profanity.tsx
+++ my-app/src/Profanity.tsx
@@ -1,54 +1,79 @@
 import {useState} from "react";
-import { useNavigate } from 'react-router-dom';
 
 
 const Create = () => {
     const [link, setLink] = useState('');
     const [isPending, setIsPending] = useState(false);
+    const [infoRetrieved, setInfoRetrieved] = useState(false);
     const [error, setError] = useState(null)
 
-    const navigate = useNavigate();
 
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
+    const handleDownload = async (html: boolean, img: boolean, e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        const profanityInfo = {link};
 
+        try {
+            const response = await fetch('http://localhost:5000/api/profanity-download', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    link: {link},
+                    html: html,
+                    img: img,
+                })
+            });
+
+            const data = await response.blob();
+            const href = URL.createObjectURL(data);
+
+            const a = document.createElement("a");
+
+            if (html){
+                a.download = 'clone.mhtml'
+            } else if (img) {
+                a.download = 'screenshot.png'
+            }
+
+            a.href = href;
+
+            document.body.appendChild(a);
+            a.click();
+        } catch (error: any) {
+            if (error.name === 'AbortError') {
+                console.log('Fetch aborted.');
+                return;
+            }
+        }
+    };
+
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        setInfoRetrieved(false);
         setIsPending(true);
 
-        fetch('http://localhost:5000/api/profanity', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(profanityInfo)
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('Failed to POST!');
-                }
-                return res.json();
-            })
-            .then(response => {
-                setIsPending(false);
-
-                response.file().then((file: Blob | MediaSource) => {
-                    let url = window.URL.createObjectURL(file);
-                    let a = document.createElement('a');
-                    a.href = url;
-                    a.download = 'employees.json';
-                    a.click();
-                });
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log('Fetch aborted.');
-                    return;
-                }
-                setError(err.message);
-                setIsPending(false);
+        try {
+            await fetch('http://localhost:5000/api/website-link', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({link})
             });
+
+            setIsPending(false);
+            setInfoRetrieved(true);
+            setError(null);
+        } catch (error: any) {
+            if (error.name === 'AbortError') {
+                console.log('Fetch aborted.');
+                return;
+            }
+            setError(error.message);
+            setIsPending(false);
+        }
     }
 
     return (
-        <div className="profanity">
+        <div className="submit-form">
             <h2>Profanity Filter</h2>
             <form onSubmit={handleSubmit}>
                 <label>Link:</label>
@@ -62,8 +87,16 @@ const Create = () => {
                 {isPending && <button disabled>Fetching Info...</button>}
                 {error && <p>{error}</p>}
             </form>
+            {infoRetrieved &&
+                <div className="profanity-data">
+                    <h1>Profanity Report</h1>
+                    <button onClick={(e) => handleDownload(true, false, e)}>HTML</button>
+                    <button onClick={(e) => handleDownload(false, true, e)}>IMAGE</button>
+                </div>
+            }
         </div>
     )
 }
+
 
 export default Create;
