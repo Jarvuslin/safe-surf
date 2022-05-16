c4fc83103b66f28ba22ff8c5302567ddac80f04e
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-05-15 21:40:35 -0400

    file sending

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,17 +7,40 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth"
 import ErrnoException = NodeJS.ErrnoException;
 import {performance} from 'perf_hooks';
+import {fileURLToPath} from "url";
+import path from "path";
 
-dotenv.config();
+// file pathing
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// .env setup
+dotenv.config({path: path.join(__dirname, "..", ".env")});
+
+// stealth plugin setup
 puppeteer.use(StealthPlugin());
 
+// express setup
 const app = express()
 app.use(cors())
 
-
+// body parser setup
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
+
+
+// create folder if it doesn't exist
+const folderPath = path.join(__dirname, "..", "clones");
+if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+}
+
+// Allocating dictionary to memory
+let badWords: string[] = []
+fs.readFile('server/dictionary/bad-words', function (err: ErrnoException | null, data: { toString: () => string; }) {
+    if (err) throw err;
+    badWords = data.toString().replace(/\r\n/g, '\n').split('\n');
+});
 
 
 function profanityReport(wordCount: number, profanityCount: number) {
@@ -29,13 +52,6 @@ function profanityReport(wordCount: number, profanityCount: number) {
 }
 
 
-// Allocating bad words to memory on launch
-let badWords: string[] = []
-fs.readFile('server/dictionary/bad-words', function (err: ErrnoException | null, data: { toString: () => string; }) {
-    if (err) throw err;
-    badWords = data.toString().replace(/\r\n/g, '\n').split('\n');
-});
-
 async function profanityData(link: string) {
     const browser = await puppeteer.launch({
         headless: false
@@ -43,16 +59,13 @@ async function profanityData(link: string) {
     const page = await browser.newPage();
     await page.goto(link);
 
-    // time
     const startTime = performance.now()
 
-    let i = 0
 
     let newHtml = await page.evaluate(() => document.body.innerHTML)
 
     for (let badWord of badWords) {
         newHtml = newHtml.replace(new RegExp(`\\b${badWord}\\b`, "gi"), '*'.repeat(badWord.length));
-        console.log(i++)
     }
 
     await page.evaluate((newHtml) => document.body.innerHTML = newHtml, newHtml)
@@ -62,8 +75,9 @@ async function profanityData(link: string) {
 
     await page.screenshot({path: 'server/clones/screenshot.png', fullPage: true});
 
-    await fs.writeFile('server/clones/clone.html', data, "utf-8", function (err: ErrnoException | null) {
-        if (err) throw err;
+    fs.writeFile('server/clones/clone.mhtml', data, "utf-8", function (err: ErrnoException | null) {
+        if (err)
+            throw err;
     });
 
     // time
@@ -89,17 +103,32 @@ async function profanityData(link: string) {
 }
 
 
-
-app.post('/api/profanity', async function (req, res) {
-    console.log(await req.body)
+app.post('/api/website-link', async function (req, res) {
+    console.log(`Link: ${await req.body.link}`)
 
     const { link } = req.body
-
     await profanityData(link)
 
-    return res.sendFile(__dirname + '/clones/screenshot.png')
-    // res.sendFile(__dirname + '/clones/clone.html')
+    res.send({
+        status: 'success'
+    })
+
+    console.log('Profanity processing completed')
 })
 
-app.listen(5000);
-console.log('Server running on port 5000');
+
+app.post('/api/profanity-download', async function (req, res) {
+    let fileName: string
+
+    if (req.body.html){
+        fileName = 'clone.mhtml'
+    } else if (req.body.img) {
+        fileName = 'screenshot.png'
+    }
+
+    res.download(path.join(__dirname, '..', `/clones/${fileName!}`), 'clone.mhtml');
+})
+
+
+app.listen(process.env.PORT || 3000,
+    () => console.log(`Listening on port ${process.env.PORT || 3000}`))
