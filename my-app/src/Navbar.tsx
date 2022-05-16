ee26b3bd7adc18f48b274f42fb991dc42a800a15
Ghaith Khalil <ghaithkale@gmail.com>
2022-05-16 09:38:33 -0400

    temp changes to text

--- my-app/src/Navbar.tsx
+++ my-app/src/Navbar.tsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link to="/"><h1>SafeSurf</h1></Link>
+            <Link to="/"><h1>LOGO</h1></Link>
             <div className="li">
-                <Link to="/profanity">Profanity Filter</Link>
+                <Link to="/profanity">Search</Link>
                 <Link to="/about-us">About Us</Link>
                 <Link to="/contact-us">Contact Us</Link>
             </div>
