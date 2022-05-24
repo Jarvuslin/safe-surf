179b34e3a0b4665c0f654fe58817c35179031e53
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-05-24 01:37:42 -0400

    Dark Theme

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Profanity from './Profanity';
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+    typography: {
+        fontFamily: 'Akshar',
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 600,
+    },
+});
 
 const App = () => {
-  return (
-      <Router>
-        <div className="App">
-          <Navbar/>
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/profanity" element={<Profanity/>}/>
-              <Route path="/about-us" element={<AboutUs/>}/>
-              <Route path="/contact-us" element={<ContactUs/>}/>
-            </Routes>
-          </div>
-        </div>
-      </Router>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/search" element={<Profanity/>}/>
+                    <Route path="/about-us" element={<AboutUs/>}/>
+                    <Route path="/contact-us" element={<ContactUs/>}/>
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
