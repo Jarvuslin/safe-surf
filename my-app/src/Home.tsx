11b62e0ca5f2e07d929f26e306ef692e3b0008ad
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-05-24 21:23:51 -0400

    MUI Typography Setup

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 import Typography from '@mui/material/Typography';
 
 const Home = () => {
+    const useStyles = {
+        title: {
+            marginTop: '1rem',
+        }
+    };
+
     return (
         <div className="home">
-            <Typography variant="h1">
-                Home
+            <Typography
+                sx={useStyles.title}
+                variant='h6'
+                color='textPrimary'
+                align='center'
+                gutterBottom
+            >
+                HOME
             </Typography>
         </div>
     );
