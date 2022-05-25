5040ac8d9b7273aa3bdb7e139ba8693630b9b527
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-05-24 21:25:35 -0400

    REVAMP: MUI transition / user error handling

--- my-app/src/Profanity.tsx
+++ my-app/src/Profanity.tsx
@@ -1,13 +1,19 @@
+import * as React from 'react';
 import {useState} from "react";
-
+import LoadingButton from "@mui/lab/LoadingButton";
+import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import Paper from '@mui/material/Paper';
+import Button from "@mui/material/Button";
 
 const Profanity = () => {
     // state
     const [link, setLink] = useState('');
     const [isPending, setIsPending] = useState(false);
     const [infoRetrieved, setInfoRetrieved] = useState(false);
-    const [error, setError] = useState(null)
-
+    const [error, setError] = useState('')
     // profanity data
     const [profanityReport, setProfanityReport] = useState({
         wordCount: null,
@@ -15,13 +21,24 @@ const Profanity = () => {
         profanityMakeup: null
     });
 
+    const useStyles = {
+        title: {
+            marginTop: '1rem',
+        },
+        field: {
+            marginTop: 2,
+            marginBottom: 2,
+            display: 'block'
+        }
+    };
+
     const handleDownload = async (html: boolean, img: boolean, e: { preventDefault: () => void; }) => {
         e.preventDefault();
 
         try {
             const response = await fetch('http://localhost:3500/api/profanity-download', {
                 method: 'POST',
-                headers: { "Content-Type": "application/json" },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({
                     link: {link},
                     html: html,
@@ -34,7 +51,7 @@ const Profanity = () => {
 
             const a = document.createElement("a");
 
-            if (html){
+            if (html) {
                 a.download = 'clone.mhtml'
             } else if (img) {
                 a.download = 'screenshot.png'
@@ -60,17 +77,54 @@ const Profanity = () => {
         setInfoRetrieved(false);
         setIsPending(true);
 
+        if (link.length === 0) {
+            setError('PLEASE ENTER A LINK');
+            setIsPending(false);
+            return;
+        }
+
         try {
+            console.log('submitting 2');
+            const response: Response = await fetch('http://localhost:3500/api/link-validity', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({link})
+            });
+
+            const {status} = await response.json();
+
+            if (status === 404) {
+                setError('WEBPAGE DOES NOT EXIST');
+                setIsPending(false);
+                return;
+            }
+        } catch (error: any) {
+            if (error.name === 'AbortError') {
+                console.log('Fetch aborted.');
+                return;
+            }
+            setError(error.message);
+            setIsPending(false);
+        }
+
+        setError('');
+
+        try {
+            console.log('submitting 2');
             const response: Response = await fetch('http://localhost:3500/api/website-link', {
                 method: 'POST',
-                headers: { "Content-Type": "application/json" },
+                headers: {
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify({link})
             });
 
             setIsPending(false);
             setInfoRetrieved(true);
             setProfanityReport(await response.json());
-            setError(null);
+            setError('');
         } catch (error: any) {
             if (error.name === 'AbortError') {
                 console.log('Fetch aborted.');
@@ -82,45 +136,101 @@ const Profanity = () => {
     }
 
     return (
-        <div className="submit-form">
-            <h1>Search</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Link:</label>
-                <input
-                    type="text"
-                    required
-                    value={link}
+        <Container>
+            <Typography
+                sx={useStyles.title}
+                variant='h6'
+                component='h2'
+                color='textPrimary'
+                align='center'
+                gutterBottom
+            >
+                PROFANITY FILTER
+            </Typography>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
                     onChange={(e) => setLink(e.target.value)}
+                    sx={useStyles.field}
+                    id="filled-basic"
+                    label="WEBSITE LINK"
+                    variant="filled"
+                    error={!!error}
+                    fullWidth
                 />
-                {!isPending && <button>Submit</button>}
-                {isPending && <button disabled>Fetching Info...</button>}
-                {error && <p>{error}</p>}
+                <LoadingButton
+                    type="submit"
+                    color={error ? 'error' : 'primary'}
+                    variant="contained"
+                    fullWidth
+                    endIcon={<KeyboardArrowRightIcon/>}
+                    loading={isPending}
+                >
+                    {isPending ? 'Sending...' : 'Send'}
+                </LoadingButton>
+
+                {error && <Typography sx={useStyles.title}
+                                      variant='subtitle1'
+                                      color='error'
+                                      gutterBottom
+                >
+                    {error}
+                </Typography>}
             </form>
             {infoRetrieved &&
-                <div className="profanity">
+                <div className="profanity" style={{marginTop: 10}}>
                     <div className="profanity-report">
-                        <ul>
-                            <li>
-                                <h2>Word Count</h2>
-                                <p>{profanityReport.wordCount}</p>
-                            </li>
-                            <li>
-                                <h2>Profanity Count</h2>
-                                <p>{profanityReport.profanityCount}</p>
-                            </li>
-                            <li>
-                                <h2>Profanity Percentage</h2>
-                                <p>{profanityReport.profanityMakeup}%</p>
-                            </li>
-                        </ul>
+                        <Paper elevation={3}>
+                            <Typography
+                                sx={{...useStyles.title, paddingTop: 2}}
+                                variant='h6'
+                                component='h2'
+                                color='textPrimary'
+                                align='center'
+                                gutterBottom
+                            >
+                                REPORT
+                            </Typography>
+                            <ul id='profanity-data'>
+                                <li>
+                                    <Paper elevation={12}>
+                                        <Typography>WORD COUNT</Typography>
+                                        <Typography>{profanityReport.wordCount}</Typography>
+                                    </Paper>
+                                </li>
+                                <li>
+                                    <Paper elevation={12}>
+                                        <Typography>PROFANITY COUNT</Typography>
+                                        <Typography>{profanityReport.profanityCount}</Typography>
+                                    </Paper>
+                                </li>
+                                <li>
+                                    <Paper elevation={12}>
+                                        <Typography>PROFANITY %</Typography>
+                                        <Typography>{profanityReport.profanityMakeup}</Typography>
+                                    </Paper>
+                                </li>
+                            </ul>
+                        </Paper>
                     </div>
                     <div className="profanity-buttons">
-                        <button onClick={(e) => handleDownload(true, false, e)}>HTML</button>
-                        <button onClick={(e) => handleDownload(false, true, e)}>IMAGE</button>
+                        <Button
+                            sx={{width: `calc(50% - 0.3rem)`, marginRight: '0.3rem'}}
+                            onClick={(e) => handleDownload(true, false, e)}
+                            variant="contained"
+                        >
+                            HTML
+                        </Button>
+                        <Button
+                            sx={{width: `calc(50% - 0.3rem)`, marginLeft: '0.3rem'}}
+                            onClick={(e) => handleDownload(false, true, e)}
+                            variant="contained"
+                        >
+                            IMAGE
+                        </Button>
                     </div>
                 </div>
             }
-        </div>
+        </Container>
     )
 }
 
